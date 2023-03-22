f899460be79a3f551b0e486c72ad96a0539fcdbb
SC-V <37342773+SC-V@users.noreply.github.com>
2023-03-22 09:15:51 -0300

    Update get_parameters.py

--- get_parameters.py
+++ get_parameters.py
@@ -2,7 +2,7 @@ import json
 import streamlit as st
 
 st.markdown(f"# Get routing parameters")
-st.subheader("Specify parameters below, then copy-paste them to /manual-routing in tariff editor", anchor=None)
+st.info(f"Specify parameters below, then copy-paste them to /manual-routing in tariff editor. Need to add new sorting center? Ask in the SC chat!")
 
 country = st.selectbox('Country', ["Mexico", "Chile", "UAE"], index=0, help='Defines a timezone used for routing')
 country_timezones = {
